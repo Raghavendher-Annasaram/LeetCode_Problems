32a01276f221842a85a408ba4b979c172c3986e6
Raghavendher-Annasaram <araghavendher2@gmail.com>
2026-06-02 22:42:42 +0530

    Time: 8 ms (11.91%), Space: 47.4 MB (13.06%) - LeetHub

---
+++ 3731-find-missing-elements/3731-find-missing-elements.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<Integer> findMissingElements(int[] nums) 
+    {
+            Arrays.sort(nums);
+            Set<Integer> set=new HashSet<>();
+            List<Integer> list=new ArrayList<>();
+            for(int i=0;i<nums.length;i++)
+            {
+                set.add(nums[i]);
+            }
+            for(int i=nums[0];i<=nums[nums.length-1];i++)
+            {
+                if(!set.contains(i))
+                {
+                    list.add(i);
+                }
+            }
+        return list;
+    }
+}
