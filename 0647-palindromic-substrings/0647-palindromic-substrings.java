fc742180e6a6b8d08f6139dad1cf200257643027
Raghavendher-Annasaram <araghavendher2@gmail.com>
2026-06-29 12:23:04 +0530

    Time: 819 ms (11.5%), Space: 46.6 MB (24.44%) - LeetHub

---
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int countSubstrings(String s) 
+    {
+        int count=0;
+            for(int i=0;i<s.length();i++)
+            {
+                for(int j=i;j<s.length();j++)
+                {
+                    String b=s.substring(i,j+1);
+                    if(ispalindrome(b)) count++;
+                }
+            }
+            return count;
+    }
+    public boolean ispalindrome(String a)
+    {
+        int left=0;
+        int right=a.length()-1;
+        while(left<right)
+        {
+            if(a.charAt(left)==a.charAt(right))
+            {
+                left++;
+                right--;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
